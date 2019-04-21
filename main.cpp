1f815288aa25c503fd02539ae62c296a1cb82c88
findNextStep <findNextStep@gmail.com>
2019-04-21 20:37:10 +0800

    添加被追踪程序的运行时停止和继续

--- main.cpp
+++ main.cpp
@@ -8,76 +8,71 @@
 #include <stdio.h>
 #include <chrono>
 #include <thread>
+#include <vector>
+#include <dirent.h> // opendir
+#include <sstream>
 
-void trace_for(const int pid) {
+bool process_pause(const int pid) {
+    if(ptrace(PTRACE_ATTACH, pid, 0, 0)) {
+        fprintf(stderr, "fail to attach pid :%d\n", pid);
+        return false;
+    }
     int status;
-    while(true) {
-        wait(&status);
-        if(WIFEXITED(status)) { //子进程发送退出信号，退出循环
-            break;
-        }
-        struct user_regs_struct regs;
-        ptrace(PTRACE_GETREGS, pid, 0, &regs);
-        // int ins = ptrace(PTRACE_PEEKDATA, pid, regs.rip, 0);
-        // printf("%12llx\t%16llx\n", regs.rip,ins);
-        printf("orig_eax = %ld \n", regs.rip);
-        ptrace(PTRACE_SINGLESTEP, pid, 0, 0);
+    if(waitpid(pid, &status, __WALL) != pid || !WIFSTOPPED(status)) {
+        fprintf(stderr, "fail to wait pid :%d\n", pid);
+        return false;
+    }
+    return true;
+}
+
+void procsss_start(const int pid) {
+    if(ptrace(PTRACE_DETACH, pid, 0, 0)) {
+        fprintf(stderr, "fail to detach pid :%d\n", pid);
     }
 }
 
-void trace_fore(const int pid) {
-    long orig_eax;
-    int status;
-    while(1) {
-        //等待子进程信号
-        wait(&status);
-        if(WIFEXITED(status)) { //子进程发送退出信号，退出循环
-            break;
-        }
-        //调用ptrace从子进程取数据
-        orig_eax = ptrace(PTRACE_PEEKUSER, pid, 8 * RIP, NULL);
-        printf("orig_eax = %ld \n", orig_eax);
-        //让子进程继续执行
-        ptrace(PTRACE_SINGLESTEP, pid, 0, 0);
+static std::vector<pid_t> ListThreads(pid_t pid) {
+    std::vector<pid_t> result;
+    std::stringstream dirname;
+    dirname << "/proc/" << pid << "/task";
+    auto *dir = opendir(dirname.str().c_str());
+    if(dir == nullptr) {
+        // fail
+        return {};
     }
+    dirent *entry;
+    while((entry = readdir(dir)) != nullptr) {
+        std::string name = entry->d_name;
+        if(name[0] != '.') {
+            result.push_back(static_cast<pid_t>(std::stoi(name)));
+        }
+    }
+    return result;
 }
 
 void some_time_trace(const int pid) {
     int status;
     long orig_eax;
     while(1) {
-        ptrace(PTRACE_ATTACH, pid, 0, 0);
-        wait(&status);
-        // std::this_thread::sleep_for(std::chrono::nanoseconds(10));
-        printf("test\n");
-        // wait(&status);
-        ptrace(PTRACE_SINGLESTEP, pid, 0, 0);
-        fprintf(stdout, "after sigle step\n");
-        wait(&status);
-        printf("after wait\n");
-        if(WIFEXITED(status)) { //子进程发送退出信号，退出循环
-            break;
+        if(!process_pause(pid)) {
+            return;
         }
-        long orig_eax = ptrace(PTRACE_PEEKUSER, pid, 8 * RIP, NULL);
         ptrace(PTRACE_SINGLESTEP, pid, 0, 0);
         wait(&status);
         if(WIFEXITED(status)) { //子进程发送退出信号，退出循环
             break;
         }
-        long orig_eax2 = ptrace(PTRACE_PEEKUSER, pid, 8 * RIP, NULL);
-        ptrace(PTRACE_CONT, pid, 0, 0);
-        fprintf(stdout, "orig_eax = %ld \n", orig_eax);
-        fprintf(stdout, "orig_eax = %ld \n\n", orig_eax2);
-        ptrace(PTRACE_DETACH, pid, 0, 0);
+        long ip = ptrace(PTRACE_PEEKUSER, pid, 8 * RIP, NULL);
+        procsss_start(pid);
+        fprintf(stdout, "ip = %ld \n", ip);
     }
 }
 
 int main() {
     int child = fork();
     if(child == 0) {
-        ptrace(PT_TRACE_ME, 0, 0, 0);
-        // execl("/bin/ls", "ls");
-        execl("./a.out", "out");
+        execl("/bin/ls", "ls");
+        // execl("./a.out", "out");
     } else {
         some_time_trace(child);
     }
