d90a0ba0182983d584386692d9cf93e4b17fbcb0
findNextStep <findNextStep@gmail.com>
2019-04-26 16:52:10 +0800

    添加单步统计方式，为之后块统计结果转化为单步结果做对照

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include "orderMap.hpp"
 #include "processTrace.hpp"
+#include <nlohmann/json.hpp>
+#include <sstream>
 
-auto dump_and_trace(const int pid) {
+std::string lltoString(long long t)
+{
+    std::string result;
+    std::stringstream ss;
+    ss <<std::hex << t;
+    ss >> result;
+    return result;
+}
+
+auto _find_it(const ::ptraceProf::mapsReader::result_t &file_map, unsigned long long ip) {
+    for(const auto &item : file_map) {
+        for(const auto range : item.second) {
+            if(::ptraceProf::processProf::in_range(ip, range)) {
+                return std::make_pair(item.first, range);
+            }
+        }
+    }
+    return std::make_pair(std::string(),::ptraceProf::mapsReader::mem_range());
+}
+
+auto dump_and_trace_sign(const int pid) {
     int status = -1;
     wait(&status);
-    if(WIFEXITED(status)) { //子进程发送退出信号，退出循环
-        return ::ptraceProf::orderMap::result_t();
-    }
-    auto m = ptraceProf::orderMap::getProcessCount(pid);
-    auto range_cache = &m[0];
-    ptrace(PTRACE_SINGLESTEP, pid, 0, 0);
-    if(waitpid(pid, &status, __WALL) != pid || !WIFSTOPPED(status)) {
-        if(kill(pid, 0)) {
-            return m;
-        }
-        fprintf(stderr, "fail to wait pid :%d process may exited\n", pid);
-        fprintf(stderr, "the tracer process will continue\n");
-        return m;
-    }
-    long last_command = ptrace(PTRACE_PEEKUSER, pid, 8 * RIP, NULL);
+    auto maps = ::ptraceProf::mapsReader::readMaps(pid);
+    std::map<std::string,std::map<std::string,int> > ans;
     while(1) {
-        ptrace(PTRACE_SINGLEBLOCK, pid, 0, 0);
+        ptrace(PTRACE_SINGLESTEP, pid, 0, 0);
         if(waitpid(pid, &status, __WALL) != pid || !WIFSTOPPED(status)) {
             if(kill(pid, 0)) {
                 break;
@@ -31,38 +40,15 @@ auto dump_and_trace(const int pid) {
             break;
         }
         long ip = ptrace(PTRACE_PEEKUSER, pid, 8 * RIP, NULL);
-        bool in = false;
-        if(ip >= std::get<1>(*range_cache).start && ip <= std::get<1>(*range_cache).end) {
-            in = true;
-            std::get<2>(*range_cache)[ip - std::get<1>(*range_cache).start][last_command]++;
-            last_command = ip;
-        } else {
-            for(auto &tuple : m) {
-                if(ip >=  std::get<1>(tuple).start && ip <= std::get<1>(tuple).end) {
-                    in = true;
-                    range_cache = &tuple;
-                    std::get<2>(*range_cache)[ip - std::get<1>(*range_cache).start][last_command]++;
-                    last_command = ip;
-                }
-            }
+        auto item = _find_it(maps,ip);
+        if (item.first == ""){
+            maps = ::ptraceProf::mapsReader::readMaps(pid);
+            item = _find_it(maps,ip);
         }
-        if(!in) {
-            ptraceProf::orderMap::getProcessCount(pid, m);
-            for(auto &tuple : m) {
-                if(ip >=  std::get<1>(tuple).start && ip <= std::get<1>(tuple).end) {
-                    in = true;
-                    range_cache = &tuple;
-                    std::get<2>(*range_cache)[ip - std::get<1>(*range_cache).start][last_command]++;
-                    last_command = ip;
-                }
-            }
-            if(!in) {
-                std::cerr << "meet error rip " << ip << " in no file " << std::endl;
-                exit(1);
-            }
-        }
+        ans[item.first][lltoString(ip - item.second.start + item.second.offset)]++;
+
     }
-    return m;
+    return nlohmann::json(ans);
 }
 
 auto analize_trace(const ::ptraceProf::orderMap::result_t &result) {
@@ -105,9 +91,10 @@ auto analize_trace(const ::ptraceProf::processProf &pp) {
         auto it_start = find_it(pp.get_file_map(), item.first.first);
         auto it_end = find_it(pp.get_file_map(), item.first.second);
         // fprintf (stderr,"%lx --%d--> %lx\n",item.first.first,item.second,item.first.second);
-        cout << std::hex << item.first.first - it_start.second.start + it_start.second.offset << " " << it_start.first << "--"
-             << std::dec << item.second << "--"
-             << std::hex << item.first.second - it_end.second.start + it_end.second.offset << " " << it_end.first << "\n";
+        cout << std::hex << item.first.first - it_start.second.start + it_start.second.offset << " " << it_start.first << " "
+             << std::hex << item.first.second - it_end.second.start + it_end.second.offset << " " << it_end.first << " "
+             << std::dec << item.second << " "
+             << "\n";
     }
 }
 
@@ -120,9 +107,11 @@ int main() {
     } else {
         printf("%d\n", child);
         // some_time_trace(child);
-        // analize_trace(dump_and_trace(child));
-        ::ptraceProf::processProf pp;
-        pp.trace(child);
-        analize_trace(pp);
+        std::cerr << dump_and_trace_sign(child).dump(4);
+        // ::ptraceProf::processProf pp;
+        // pp.trace(child);
+        // analize_trace(pp);
     }
 }
+
+
