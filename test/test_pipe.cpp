8f357a948ab88cacefd9bccdac0df049368455d9
findNextStep <findNextStep@gmail.com>
2019-04-28 14:31:11 +0800

    跟进hpp修改更新测试程序

--- test/test_pipe.cpp
+++ test/test_pipe.cpp
@@ -3,15 +3,12 @@
 #include <vector>
 
 int main() {
-    // using ::ptraceProf::get_cmd_stream_;
     using ::ptraceProf::get_cmd_stream;
-    // auto fs = get_cmd_stream("echo 233");
-    auto fs = get_cmd_stream({ "/bin/sh","-c","/bin/echo 233" });
+    auto fs = get_cmd_stream("echo 233");
 
     while(fs) {
         std::string line;
         std::getline(fs,line);
         std::cout <<"line "<< line << std::endl;
     }
-    fs.close();
 }
--- test/test_readDump.cpp
+++ test/test_readDump.cpp
@@ -2,22 +2,16 @@
 #include "pipe.hpp"
 #include <iostream>
 #include <algorithm>
-auto read_n_line(::std::istream &a) {
+auto read_n_line(std::istream &a) {
     std::string s;
     int sum = 0;
     while(a) {
         std::getline(a, s);
-        sum += s.size();
+        sum++;
     }
-        std::getline(a, s);
-        sum += s.size();
-        std::getline(a, s);
-        sum += s.size();
-        std::getline(a, s);
-        sum += s.size();
     std::cout << "has line " << sum << std::endl;
 }
-auto read_n_line(::ptraceProf::pipstream &&a) {
+auto read_n_line(std::istream &&a) {
     if(a) {
         return read_n_line(a);
     }
@@ -31,29 +25,32 @@ int main() {
     using std::endl;
     // read_n_line(get_cmd_stream_({"/bin/echo","echo 233"}));
 
-    read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libc-2.27.so"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libgcc_s.so.1"));
-    read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
-    read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libc-2.27.so"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libgcc_s.so.1"));
+    // read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libc-2.27.so"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libgcc_s.so.1"));
+    // read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
+    // read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libc-2.27.so"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libgcc_s.so.1"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
+    // read_n_line(get_cmd_stream("objdump -d /usr/lib/x86_64-linux-gnu/libstdc++.so.6"));
     read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
-    read_n_line(get_cmd_stream("objdump -d /usr/lib/x86_64-linux-gnu/libstdc++.so.6"));
+    cout << endl;
     read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
+    cout << endl;
     // return 0;
     cout << "size " <<read_objdump(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out")).size() << endl;
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libc-2.27.so"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libgcc_s.so.1"));
-    read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
-    read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libc-2.27.so"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libgcc_s.so.1"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
-    read_n_line(get_cmd_stream("objdump -d /usr/lib/x86_64-linux-gnu/libstdc++.so.6"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
-    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
+    cout << endl;
+    read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so",false));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libgcc_s.so.1"));
+    // read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
+    // read_n_line(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libc-2.27.so"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libgcc_s.so.1"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
+    // read_n_line(get_cmd_stream("objdump -d /usr/lib/x86_64-linux-gnu/libstdc++.so.6"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
+    // read_n_line(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libm-2.27.so"));
     // return 0;
     cout << read_objdump(get_cmd_stream("objdump -d /home/pxq/final_design/ptrace_prof/a.out")).size() << endl;
     cout << read_objdump(get_cmd_stream("objdump -d /lib/x86_64-linux-gnu/libc-2.27.so")).size() << endl;
