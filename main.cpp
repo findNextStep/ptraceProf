d3e95801ca2cba5dd6aeb3ca4ccd31f8e04b7afd
findNextStep <findNextStep@gmail.com>
2019-05-06 21:11:39 +0800

    使用自己写的参数解析满足需求

--- main.cpp
+++ main.cpp
@@ -1,74 +1,60 @@
 #include "processTrace.hpp"
+#include "afterProcess.hpp"
 #include <iostream>
 #include <nlohmann/json.hpp>
-#include "gethin.hpp"
 
-int main(int argc, char **argv) {
-    gethin::Flag single_step = gethin::Flag()
-                               .shortOpt('s')
-                               .longOpt("single_step")
-                               .help("decisiion whether use single step test");
-    gethin::String step_file = gethin::String()
-                               .longOpt("step-file")
-                               .help("output file for process step. If no set ,while output to stderr");
-    gethin::String final_result_file = gethin::String()
-                                       .shortOpt('r')
-                                       .longOpt("final_result_file")
-                                       .help("the file for ans as {file:[offset,time]} in json. If no set, won`t output");
-    gethin::String exec_path = gethin::String()
-                               .longOpt("exec_path")
-                               .shortOpt('c')
-                               .help("exeable file path. will be ./a.out in default");
-    gethin::OptionReader optReader({
-        &single_step,
-        &step_file,
-        &final_result_file,
-        &exec_path,
-    }, "ptraceProf");
-    if(argc > 1) {
-        try {
-            optReader.read(argc, argv);
-        } catch(const std::invalid_argument &e) {
-            std::cerr << e.what() << std::endl;
-            return 1;
-        } catch(...) {
-            std::cerr << "Error during execution!" << std::endl;
+int main(const int argc, char *argv[]) {
+    std::string step_file, final_result_file;
+    bool single_step = false;
+    const std::vector<std::string> args(argv, argv + argc);
+    unsigned int last_command_pos = argc;
+    for(unsigned int i = 1; i < args.size(); ++i) {
+        const std::string arg = args[i];
+        if(arg == "-c"
+                || arg == "--single-step") {
+            single_step = true;
+        } else if(arg == "-e"
+                  || arg == "--exec") {
+            last_command_pos = i + 1;
+            break;
+        } else if(arg == "-s"
+                  || arg == "--step-file") {
+            step_file = args[++i];
+        } else if(arg == "-f"
+                  || arg == "--final-file") {
+            final_result_file = args[++i];
+        } else {
+            std::cerr << arg << std::endl;
             return 1;
         }
     }
-
     int child = fork();
     if(child == 0) {
         // in tracee process
         ::ptraceProf::ptraceThisProcess();
-        if(exec_path.value().empty()) {
-            execl("./a.out", "out");
+        std::string process_name = "tracee with ";
+        if(argc == last_command_pos) {
+            execlp("./a.out", "out");
         } else {
-            std::string process_name = "tracee with ";
-            if(single_step.value()) {
-                process_name += "single";
-            } else {
-                process_name += "block";
-            }
-            execl(exec_path.value().c_str(), process_name.c_str());
+            execvp(argv[last_command_pos], argv + last_command_pos);
         }
     }
     // in tracer porcess
     std::cout << "child_pid == " << child;
     // command line value check
-    if(step_file.value().size()) {
-        freopen(step_file.value().c_str(), "w", stderr);
+    if(step_file.size()) {
+        freopen(step_file.c_str(), "w", stderr);
     }
     ::ptraceProf::processProf pp;
 
     std::map<std::string, std::map<std::string, ::ptraceProf::count_t> > ans;
-    if(single_step.value()) {
+    if(single_step) {
         std::cout << "\tin single step" << std::endl;
         auto [count, maps] = ::ptraceProf::dump_and_trace_sign(child);
         std::cout << "finish" << std::endl;
-        if(final_result_file.value().size()) {
+        if(final_result_file.size()) {
             ans = ::ptraceProf::analize(maps, count);
-            std::ofstream of(final_result_file.value());
+            std::ofstream of(final_result_file);
             of << ::nlohmann::json(ans).dump(4);
         } else {
             ans = ::ptraceProf::analize(maps, count);
@@ -78,14 +64,19 @@ int main(int argc, char **argv) {
         std::cout << "\tin block step" << std::endl;
         pp.trace(child);
         std::cout << "finish" << std::endl;
-        if(final_result_file.value().size()) {
+        if(final_result_file.size()) {
             ans = pp.analize();
-            std::ofstream of(final_result_file.value());
+            std::ofstream of(final_result_file);
             of << ::nlohmann::json(ans).dump(4);
         } else {
             ans = pp.analize();
             std::cerr << ::nlohmann::json(ans).dump(4);
         }
+    }
+    std::ofstream of("test.final.json");
+    for(auto [name, time] : ::ptraceProf::order_output_function(ans)) {
+        of << name << '\t' << time << '\n';
+
     }
     return 0;
 }
