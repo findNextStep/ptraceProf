10cd7a1596a4ff37338a5d6f0393ac2b645fea8d
findNextStep <findNextStep@gmail.com>
2019-05-06 23:57:34 +0800

    添加简易的对比程序

---
+++ lanuch_test.sh
@@ -0,0 +1,23 @@
+step_file="test.sign.step"
+ans_file="test.sign.json"
+addre_file="test.sign.addre"
+func_file="test.sign.func"
+
+rm $step_file
+rm $ans_file
+rm $addre_file
+rm $func_file
+
+x-terminal-emulator -x "zsh -c \"./build/main --single-step --step-file $step_file --final-file $ans_file --addre-file $addre_file --func-file $func_file -e $@\" "&
+
+step_file="test.block.step"
+ans_file="test.block.json"
+addre_file="test.block.addre"
+func_file="test.block.func"
+
+rm $step_file
+rm $ans_file
+rm $addre_file
+rm $func_file
+
+ x-terminal-emulator -x "zsh -c \"./build/main               --step-file $step_file --final-file $ans_file --addre-file $addre_file --func-file $func_file -e $@\"" &
