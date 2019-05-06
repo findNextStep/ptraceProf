cde9d54e7bf137163450d07c39b0b86b93a42720
findNextStep <findNextStep@gmail.com>
2019-05-06 23:47:07 +0800

    将后续处理添加到命令行参数中

--- main.cpp
+++ main.cpp
@@ -5,24 +5,25 @@
 
 int main(const int argc, char *argv[]) {
     std::string step_file, final_result_file;
+    std::string addre_count_file, function_count_file;
     bool single_step = false;
     const std::vector<std::string> args(argv, argv + argc);
     unsigned int last_command_pos = argc;
     for(unsigned int i = 1; i < args.size(); ++i) {
         const std::string arg = args[i];
-        if(arg == "-c"
-                || arg == "--single-step") {
+        if(arg == "-c" || arg == "--single-step") {
             single_step = true;
-        } else if(arg == "-e"
-                  || arg == "--exec") {
+        } else if(arg == "-e" || arg == "--exec") {
             last_command_pos = i + 1;
             break;
-        } else if(arg == "-s"
-                  || arg == "--step-file") {
+        } else if(arg == "-s" || arg == "--step-file") {
             step_file = args[++i];
-        } else if(arg == "-f"
-                  || arg == "--final-file") {
+        } else if(arg == "-f" || arg == "--final-file") {
             final_result_file = args[++i];
+        } else if(arg == "--addre-file") {
+            addre_count_file = args[++i];
+        } else if(arg == "--func-file") {
+            function_count_file = args[++i];
         } else {
             std::cerr << arg << std::endl;
             return 1;
@@ -73,10 +74,17 @@ int main(const int argc, char *argv[]) {
             std::cerr << ::nlohmann::json(ans).dump(4);
         }
     }
-    std::ofstream of("test.final.json");
-    for(auto [name, time] : ::ptraceProf::order_output_function(ans)) {
-        of << name << '\t' << time << '\n';
-
+    if(function_count_file.size()) {
+        std::ofstream of(function_count_file);
+        for(auto [name, time] : ::ptraceProf::order_output_function(ans)) {
+            of << name << '\t' << time << '\n';
+        }
+    }
+    if (addre_count_file.size()){
+        std::ofstream of(addre_count_file);
+        for(auto [name, time] : ::ptraceProf::order_output(ans)) {
+            of << name << '\t' << time << '\n';
+        }
     }
     return 0;
 }
