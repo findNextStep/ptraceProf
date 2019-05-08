8a974090c3a3c813adf6656281616b029190954e
findNextStep <findNextStep@gmail.com>
2019-05-08 19:40:25 +0800

    消除读取命令的错误输出

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -2,14 +2,14 @@
 
 namespace ptraceProf {
 
-auto exec(const char* cmd) {
+auto exec(const char *cmd) {
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
+    if(!pipe) {
         throw std::runtime_error("popen() failed!");
     }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    while(fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
         result += buffer.data();
     }
     return std::stringstream(result);
@@ -17,7 +17,7 @@ auto exec(const char* cmd) {
 
 
 std::stringstream get_cmd_stream(const std::string &&cmd) {
-    return exec(cmd.c_str());
+    return exec((cmd + " 2> /dev/null").c_str());
 }
 
 
