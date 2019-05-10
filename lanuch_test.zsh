9a571a1f36353d4ce42d3b04c4e8920f80b1c2cb
findNextStep <findNextStep@gmail.com>
2019-05-10 16:04:37 +0800

    更改格式

--- lanuch_test.zsh
+++ lanuch_test.zsh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 rm test.time 
 echo "real\tuser\tsys" >> test.time
-max_i=1000000
-readonly gap=100000
+max_i=10000000
+readonly gap=1000000
 for ((i=100000;i<max_i;i=i+gap)){
     echo -n $i >> test.time
-    /usr/bin/time -o test.time -a -p -f "\t%E\t%U\t%S" ./a.out $i
+    /usr/bin/time -o test.time -a -p -f "\t%e" ./a.out $i
 }
 for ((i=100000;i<max_i;i=i+gap)){
     echo -n $i >> test.time
-    /usr/bin/time -o test.time -a -p -f "\t%E\t%U\t%S" ./build/main -e ./a.out $i
+    /usr/bin/time -o test.time -a -p -f "\t%e" ./build/main -e ./a.out $i
 }
 
 return 0;
