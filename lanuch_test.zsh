a9be934e32f21f6b5110637834017bc3a6318117
findNextStep <findNextStep@gmail.com>
2019-05-11 09:46:24 +0800

    分行写，保证可读性

--- lanuch_test.zsh
+++ lanuch_test.zsh
@@ -2,7 +2,11 @@
 max_i=10000000
 readonly gap=1000000
 for ((i=100000;i<max_i;i=i+gap)){
-    /usr/bin/time -o test.$i.time -a -p -f "\t%e" ./build/main -d test.$i.addre -f test.$i.func -e ./a.out $i
+    /usr/bin/time -o test.$i.time -a -p -f "\t%e" \
+        ./build/main -d test.$i.addre \
+        -f test.$i.func \
+        -r test.$i.json \
+        -e ./a.out $i&
 }
 
 return 0;
