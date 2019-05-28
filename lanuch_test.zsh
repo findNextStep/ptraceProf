9a8f4fb027998ab2749814165563bc68c629733d
findNextStep <findNextStep@gmail.com>
2019-05-28 14:48:59 +0800

    update

--- lanuch_test.zsh
+++ lanuch_test.zsh
@@ -1,12 +1,14 @@
 #!/bin/zsh
-max_i=10000000
+max_i=100000000
 readonly gap=1000000
-for ((i=100000;i<max_i;i=i+gap)){
-    /usr/bin/time -o test.$i.time -a -p -f "\t%e" \
-        ./build/main -d test.$i.addre \
-        -f test.$i.func \
-        -r test.$i.json \
-        -e ./a.out $i&
+for ((i=gap;i<max_i;i=i+gap)){
+    /usr/bin/time -o test.d.time -a -p -f "\t%e" \
+        ./build/main \
+        -e ./a.out $i
+        # -e ./a.out $i&
+        # -d test.$i.addre \
+        # -f test.$i.func \
+        # -r test.$i.json \
 }
 
 return 0;
